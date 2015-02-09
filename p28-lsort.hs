c05cc8a57f67aa3a4d0670b009abf2b7c38cdc21
arun <arunrv2011@gmail.com>
2015-02-10 02:30:25 +0530

    A more compact version of p28, lsort

--- p28-lsort.hs
+++ p28-lsort.hs
@@ -6,13 +6,17 @@ import qualified Data.Map as M
 lsort :: [[a]] -> [[a]]
 lsort xs = sortBy (\xs' ys' -> compare (length xs') (length ys')) xs
 
+ --sorter :: M.Map Int Int -> [a] -> [a] -> Orderig
+sorter fm xs ys = let xslen = length xs
+                      yslen = length ys in
+                  compare ((fm M.! xslen), xslen) ((fm M.! yslen), yslen) 
+
 -- Problem 28-b
 -- Sort a list of lists by the frequency of the lengths of their elements
 lsortf :: [[a]] -> [[a]]
 lsortf xs = let addLenFreqToMap len map = if (M.member len map) then (M.insert len ((map M.! len) + 1) map) 
                                           else (M.insert len 0 map)
-                freqMap = foldr addLenFreqToMap M.empty (map length xs)
-                sorter fm xs' ys' = compare ((fm M.! length xs'), length xs') ((fm M.! length ys'), length ys') in
+                freqMap = foldr addLenFreqToMap M.empty (map length xs) in
             sortBy (sorter freqMap) xs
 
 -- Problem 28-b
@@ -23,7 +27,6 @@ pack ys@(x:xs) = (x, length $ takeWhile (== x) ys):(pack (dropWhile (== x) ys))
 
 lsortf2 :: [[a]] -> [[a]]
 lsortf2 xs = let lenFreqs = pack . sort $ map length xs 
-                 freqMap = M.fromList lenFreqs
-                 sorter fm xs' ys' = compare ((fm M.! length xs'), length xs') ((fm M.! length ys'), length ys') in
+                 freqMap = M.fromList lenFreqs in
              sortBy (sorter freqMap) xs
 
