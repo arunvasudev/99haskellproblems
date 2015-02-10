be62a6ea7833935a397135a664df669f146bcc0d
arun <arunrv2011@gmail.com>
2015-02-11 02:44:28 +0530

    p32 - gcd'

--- p31-41-arithmetic.hs
+++ p31-41-arithmetic.hs
@@ -3,3 +3,9 @@
 isPrime :: Int -> Bool
 isPrime n = let intSqrt = ceiling . sqrt . fromIntegral $ n in
                 if (n == 2) then True else not . any (\n' -> n `mod` n' == 0) $ [2..intSqrt]
+
+-- Problem 32
+-- Get the GCD of two integers
+gcd' :: Int -> Int -> Int
+gcd' a 0 = a
+gcd' a b = gcd' b (a `mod` b)
