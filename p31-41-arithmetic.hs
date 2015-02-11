603200aa1ac446af6dd11054976d1ed802258179
arun <arunrv2011@gmail.com>
2015-02-12 00:55:53 +0530

    Included more arithmetic problems

--- p31-41-arithmetic.hs
+++ p31-41-arithmetic.hs
@@ -1,3 +1,5 @@
+import qualified Util as U
+
 -- Problem 31
 -- Determine whether a number is prime
 isPrime :: Int -> Bool
@@ -9,3 +11,64 @@ isPrime n = let intSqrt = ceiling . sqrt . fromIntegral $ n in
 gcd' :: Int -> Int -> Int
 gcd' a 0 = a
 gcd' a b = gcd' b (a `mod` b)
+
+-- Problem 33
+coprime :: Int -> Int -> Bool
+coprime a b = (gcd a b) == 1
+
+-- Problem 34
+-- calculate the Euler's totient function for n
+totientPhi :: Int -> Int
+totientPhi n = length . filter (coprime n) $ [1..n]
+
+-- get all the prime numbers in a given range
+primes :: Int -> Int -> [Int]
+primes a b = filter isPrime [a..b]
+
+-- if k is the maximum power of p that divides n, returns a list containing
+-- p k times
+sameDivisors :: Int -> Int -> Int -> [Int]
+sameDivisors n p acc = if (n `mod` acc == 0) then p:(sameDivisors n p (acc * p)) else [] 
+
+-- problem 35
+-- determine the prime factors of a given number in ascending order
+primeFactors :: Int -> [Int]
+primeFactors n = concat [sameDivisors n p p | p <- primes 2 n]
+
+-- problem 36
+-- returns the prime factors along with their multiplicity
+primeFactorsMult :: Int -> [(Int, Int)]
+primeFactorsMult n = U.encode . primeFactors $ n
+
+-- problem 37
+-- Euler's totient function - improved
+totientPhiImp :: Int -> Int
+totientPhiImp n = product . map (\(p, k) -> (U.pow p (k - 1)) * (p - 1)) . primeFactorsMult $ n
+
+-- problem 39
+-- Given a range of numbers, construct a list of primes in that range
+primesR = primes
+
+-- problem 40
+-- print a pair of primes that sum to a given number
+goldbach :: Int -> (Int, Int)
+goldbach n = let ps = primesR 2 n 
+                 gbHelper [] n = error "Search failed"
+                 gbHelper (p:ps') n = if (isPrime (n - p)) then (p, n - p) else gbHelper ps' n in
+             gbHelper ps n
+
+-- returns the list of all goldbach primes for a given number
+goldbachList' :: Int -> [(Int, Int)]
+goldbachList' n = let ps = primesR 2 (n `div` 2) 
+                      gblHelper [] n = []
+                      gblHelper (p:ps') n = if (isPrime (n - p)) 
+                                            then (p, n - p):(gblHelper ps' n) 
+                                            else gblHelper ps' n in
+                  gblHelper ps n
+
+goldbachList'' :: Int -> [(Int, (Int, Int))]
+goldbachList'' n = [(e, pair) | e <- filter (\x -> x `mod` 2 == 0) [3..n], pair <- goldbachList' e]
+
+goldbachListLim :: Int -> Int -> [String]
+goldbachListLim n lim = [(show e) ++ " = " ++ (show p1) ++ " + " ++ (show p2)|(e, (p1, p2)) <- goldbachList'' n, p1  > lim && p2 > lim]
+
