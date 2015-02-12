c1f3caeffc49d3581fb1a640980e1cb06b74d239
arun <arunrv2011@gmail.com>
2015-02-13 01:41:38 +0530

    p48 - general truth table generation

--- p46-48-logic.hs
+++ p46-48-logic.hs
@@ -1,3 +1,5 @@
+import Util (justifyShow)
+
 -- Functions for boolean operators
 and' = (&&)
 or' = (||)
@@ -5,14 +7,14 @@ not' = not
 nand a b = not (a && b)
 nor a b = not (a || b)
 
-xor :: Bool -> Bool -> Bool
-xor a b = not (a == b)
+xor' :: Bool -> Bool -> Bool
+xor' a b = not (a == b)
 
-impl :: Bool -> Bool -> Bool
-impl a b = (not b) || a
+impl' :: Bool -> Bool -> Bool
+impl' a b = (not b) || a
 
-equ :: Bool -> Bool -> Bool
-equ = (==)
+equ' :: Bool -> Bool -> Bool
+equ' = (==)
 
 -- Problem 46
 -- Print the truth table for a given function
@@ -23,3 +25,16 @@ table2Print :: (Bool -> Bool -> Bool) -> IO ()
 table2Print f = putStr . unlines $ (table2 f) 
 
 -- Problem 47 doesn't need any modification from problem 46
+
+-- Generates all lists of boolean values of length n
+boolN :: Int -> [[Bool]]
+boolN 0 = [[]]
+boolN n = let subList = boolN (n - 1) in
+          (map (True:) subList) ++ (map (False:) subList)
+
+tablen :: Int -> ([Bool] -> Bool) -> [String]
+tablen n f = let rowFn bools = concat . map ((++ "  ") . (\b -> justifyShow b 6)) $ bools in
+             [(rowFn bs) ++ (justifyShow (f bs) 6) | bs <- boolN n]
+
+tablenPrint :: Int -> ([Bool] -> Bool) -> IO ()
+tablenPrint n f = putStr . unlines $ (tablen n f)
