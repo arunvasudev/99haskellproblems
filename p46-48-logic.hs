66ef1a671054253efe9379bf930554c59dece190
arun <arunrv2011@gmail.com>
2015-02-13 01:22:14 +0530

    p47 is solved in p46

--- p46-48-logic.hs
+++ p46-48-logic.hs
@@ -14,10 +14,12 @@ impl a b = (not b) || a
 equ :: Bool -> Bool -> Bool
 equ = (==)
 
--- Problem 46 
+-- Problem 46
 -- Print the truth table for a given function
 table2 :: (Bool -> Bool -> Bool) -> [String]
 table2 f = [ (show a) ++ "  " ++ (show b) ++ "  " ++ (show (f a b)) | a <- [True, False], b <- [True, False]]
 
 table2Print :: (Bool -> Bool -> Bool) -> IO ()
 table2Print f = putStr . unlines $ (table2 f) 
+
+-- Problem 47 doesn't need any modification from problem 46
