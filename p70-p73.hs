8f0a50ca82092a273f85bc169c7043f127ee511d
arun <arunrv2011@gmail.com>
2015-09-06 21:18:30 +0530

    P73A - convert a tree to a lispy string

--- p70-p73.hs
+++ p70-p73.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Tree a = Node a [Tree a] deriving (Eq, Show)
 
 -- test data
@@ -39,3 +41,10 @@ ipl' plSoFar (Node a xs) =
 -- P72 - create a bottom up sequence of the nodes of a multi-way tree
 bottomUp :: Tree Char -> String
 bottomUp (Node c xs) = (concat [bottomUp t | t <- xs]) ++ [c]
+
+-- P73A - get the lispy string representation from a multiway tree
+toLispyString :: Tree Char -> [Char]
+toLispyString tree = 
+    case tree of
+        Node c [] -> [c]
+        Node c xs -> '(':c:' ':(concat (intersperse " " [toLispyString t | t <- xs])) ++ ")"
